315196df131efa396945b2a3d838d0b14d702729
Margot Liard <margliard@gmail.com>
2021-11-23 14:41:45 +0100

    setup model favorite

---
+++ app/models/favorite.rb
@@ -0,0 +1,4 @@
+class Favorite < ApplicationRecord
+  belongs_to :experience
+  belongs_to :travelboard
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_11_23_110123) do
-
+ActiveRecord::Schema.define(version: 2021_11_23_120944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "travelboards", force: :cascade do |t|
-    t.string "name"
-    t.boolean "status"
-    t.date "start_date"
-    t.date "end_date"
-    t.string "country"
 
   create_table "experiences", force: :cascade do |t|
     t.string "category"
@@ -37,7 +27,44 @@ ActiveRecord::Schema.define(version: 2021_11_23_110123) do
     t.float "longitude"
     t.string "description"
     t.boolean "booked"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
+  create_table "favorites", force: :cascade do |t|
+    t.date "assigned_day"
+    t.bigint "experience_id", null: false
+    t.bigint "travelboard_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["experience_id"], name: "index_favorites_on_experience_id"
+    t.index ["travelboard_id"], name: "index_favorites_on_travelboard_id"
+  end
+
+  create_table "review_experiences", force: :cascade do |t|
+    t.text "comment"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_review_experiences_on_user_id"
+  end
+
+  create_table "review_travelboards", force: :cascade do |t|
+    t.text "comment"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_review_travelboards_on_user_id"
+  end
+
+  create_table "travelboards", force: :cascade do |t|
+    t.string "name"
+    t.boolean "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -54,4 +81,8 @@ ActiveRecord::Schema.define(version: 2021_11_23_110123) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "experiences"
+  add_foreign_key "favorites", "travelboards"
+  add_foreign_key "review_experiences", "users"
+  add_foreign_key "review_travelboards", "users"
 end
