6ad2843de399b10df32f9ff39f1441986f74e62a
Margot Liard <margliard@gmail.com>
2021-11-25 12:37:22 +0100

    search bar with city and country

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,20 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// ./packs/application.js
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,8 +3,8 @@ class Experience < ApplicationRecord
   has_many :favorites
 
   include PgSearch::Model
-  pg_search_scope :search_by_city,
-    against: [:city],
+  pg_search_scope :search_by_city_and_country,
+    against: [ :city, :country ],
     using: {
       tsearch: { prefix: true }
     }
