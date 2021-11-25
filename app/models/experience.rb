9875a8382cfcad433b2d4399a3901985d0fc0a7d
camille breillot <camille.breillot@gmail.com>
2021-11-25 10:25:27 +0100

    little changes

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,12 +2,12 @@ class Experience < ApplicationRecord
   has_many :review_experiences
   has_many :favorites
 
-  include PgSearch::Model
-  pg_search_scope :search_by_city,
-    against: [:city],
-    using: {
-      tsearch: { prefix: true }
-    }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_city,
+  #   against: [:city],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
