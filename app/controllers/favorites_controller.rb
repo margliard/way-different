aa3ae4bf8fc5c9a3666fef1634e61a09b7fd349d
camille breillot <camille.breillot@gmail.com>
2021-11-30 17:54:53 +0100

    travelboard-with-drag-and-drop-finish

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -22,6 +22,7 @@ class FavoritesController < ApplicationController
     @travelday = Travelday.find(params[:travelday])
     @favorite.travelday = @travelday
     @favorite.save
+    authorize @favorite
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,15 +32,9 @@ initStarRating();
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-<<<<<<< HEAD
-//document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-=======
 // document.addEventListener('turbolinks:load', () => {
 //   // Call your functions here, e.g:
 //   // initSelect2();
->>>>>>> master
 // });
 
 // ./packs/application.js
