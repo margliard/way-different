7456572b6d60f394dfdbfcce9665e5247b68e49f
sophie callault <s.callault@gmail.com>
2021-11-30 15:00:08 +0100

    little card commen ton show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,10 +32,10 @@ initStarRating();
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your functions here, e.g:
+//   // initSelect2();
+// });
 
 // ./packs/application.js
 
